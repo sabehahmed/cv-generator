c1ee22dc653c57146a42f572b96903ec6fd5c6c7
sabeh ahmed <132754845+sabehahmed@users.noreply.github.com>
2025-04-29 19:36:37 +0100

    Initial commit

---
+++ components/utility/DefaultResumeData.jsx
@@ -0,0 +1,89 @@
+const DefaultResumeData = {
+  name: "sabeh sarah",
+  position: "Developer",
+  contactInformation: "+216 20725605",
+  email: " sabehsarah@gmail.com",
+  address: ", Tunis",
+  profilePicture: "",
+  socialMedia: [
+    {
+      socialMedia: "Github",
+      link: "github.com// ",
+    },
+    {
+      socialMedia: "LinkedIn",
+      link: "linkedin.com/in/ ",
+    },
+    {
+      socialMedia: "Website",
+      link: "bedivere-lea.github.io",
+    },
+  ],
+  summary: "Resourceful and versatile Developer with solid experience in IT systems development, DevOps, and cloud infrastructure supervision. Skilled in full-stack development, process automation, CI/CD implementation, and monitoring solutions deployment.",
+  education: [
+    {
+      "school": "Higher Institute of Technological Studies of Zaghouan",
+      "degree": "Applied Bachelor's Degree in Computer Technologies ",
+      "startYear": "2021-09-01",
+      "endYear": "2025-06-30"
+    },
+  ],
+  workExperience: [
+    {
+      "company": "Netstep IT",
+      "position": "DevOps Intern (End-of-Studies Internship)",
+      "description": "Netstep IT is a company specializing in cloud infrastructure management and DevOps solutions.",
+      "keyAchievements": "Designed and deployed a high-availability Kubernetes cluster.\nIntegrated Prometheus and Grafana for infrastructure monitoring.\nImplemented CI/CD pipelines using Jenkins.\nAutomated infrastructure provisioning and configuration using Ansible.",
+      "startYear": "2025-02-01",
+      "endYear": "2025-05-31"
+    },
+    {
+      "company": "Axe Finance",
+      "position": "QA Intern (Skill-Development Internship)",
+      "description": "Axe Finance is a software company specializing in banking and financial credit solutions.",
+      "keyAchievements": "Developed an internal dashboard using Python, HTML, CSS, and SQL Server.\nParticipated in QA testing processes and CI/CD pipeline improvements.\nContributed to application monitoring setups using Prometheus and Grafana.\nConfigured alerting through Alertmanager and integrated open-source monitoring tools.",
+      "startYear": "2024-02-01",
+      "endYear": "2024-05-31"
+    },
+    {
+      "company": "Autoliv",
+      "position": "IT Intern",
+      "description": "Autoliv is the world's largest automotive safety supplier.",
+      "keyAchievements": "Discovered industrial IT environment and teamwork culture.\nLearned IT asset management and provided technical support to users.",
+     "startYear": "2023-06-15",
+      "endYear": "2023-07-15"
+    }
+  ],
+  projects: [],
+  skills: [
+    {
+      title: "Technical Skills",
+      skills: [
+        "JavaScript", "Python", "Web Services", "C++", "HTML5", "CSS", "SQL", "User Interface", "Creativity"
+      ]
+    },
+    {
+      title: "Soft Skills",
+      skills: [
+        "Collaboration", "Problem-solving", "Communication", "Time management", "Result-oriented"
+      ]
+    },
+    {
+      title: "Additional Skills",
+      skills: [
+        "Public Speaking", "Writing", "Research"
+      ] 
+    }
+  ],
+  languages: [
+    "English",
+    "Franch",
+    "arabe",
+  ],
+  certifications: [
+    "Certified Web Professional-Web Developer",
+    "Java Development Certified Professional",
+  ],
+};
+
+export default DefaultResumeData;
